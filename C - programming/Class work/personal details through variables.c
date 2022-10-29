6ecd05f87746b9c479b2f8bc2404f00f9e945367
dubeyshivam9642 <115604495+dubeyshivam9642@users.noreply.github.com>
2022-10-29 22:29:42 +0530

    Add files via upload

---
+++ C - programming/Class work/personal details through variables.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	char name[12]="Shivam Dubey";
+	int age = 21;
+	float salary = 50.75;
+	
+	printf("Name : %s\n",name); 
+	printf("Age : %d\n",age);
+	printf("Salary : %.2f",salary);
+}
