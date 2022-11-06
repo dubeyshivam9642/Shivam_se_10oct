cd7cfb74d1ae07437b003a8514133809828d6930
dubeyshivam9642 <115604495+dubeyshivam9642@users.noreply.github.com>
2022-11-06 19:01:22 +0530

    Add files via upload

---
+++ C - programming/Class work/if.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+
+{
+	int a;
+	
+	printf("\nEnter A ");
+	scanf("%d",&a);
+	
+	
+	if(a>0)
+	{
+		printf("%d is Positive",a);
+	}
+	else{
+		printf("%d is negative",a);
+	}
+}
