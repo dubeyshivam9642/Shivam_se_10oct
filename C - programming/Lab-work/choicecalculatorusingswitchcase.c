0817805d795c3a6926a4f01e1b356bd2350166af
dubeyshivam9642 <115604495+dubeyshivam9642@users.noreply.github.com>
2022-11-08 14:18:19 +0530

    Add files via upload

---
+++ C - programming/Lab-work/choicecalculatorusingswitchcase.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	printf("Enter numbers\n");
+	scanf("%d\n%d",&a,&b);
+	printf("\nPress 1 for Adition\nPress 2 for Substraction\nPress 3 for Multiplication\nPress 4 for Division\n");
+	scanf("%d",&c);
+	
+	switch(c)
+	{
+		case 1 : printf("\nAddition of %d and %d = %d",a,b,(a+b));
+		break;
+		case 2 : printf("\nSubstraction of %d and %d = %d",a,b,(a-b));
+		break;
+		case 3 : printf("\nMultiplication of %d and %d = %d",a,b,(a*b));
+		break;	
+		case 4 : printf("\nDivision of %d and %d = %.2f",a,b,((float)a/b));
+		break;			    
+		default : printf("\nINVALID TRY AGAIN");
+		break;	
+	}
+}
