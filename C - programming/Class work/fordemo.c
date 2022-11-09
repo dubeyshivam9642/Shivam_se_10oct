f76436a843c8ca2a2a53bddbd05282f1c7ed8220
dubeyshivam9642 <115604495+dubeyshivam9642@users.noreply.github.com>
2022-11-09 19:19:11 +0530

    Add files via upload

---
+++ C - programming/Class work/fordemo.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/*
+
+for loop: 
+
+	syntax: for(initialization;condition;updation)
+			{
+				statement;
+			}
+
+*/
+
+
+void main()
+{
+	int i;
+	
+	for(i=1;i<=10;i++)
+	{
+		printf("%d\n",i);
+	}
+	
+	
+	
+}
