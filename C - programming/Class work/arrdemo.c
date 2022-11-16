52a6fe6cf2e3e8ba41f448fee0e6d4a5f3a0472d
dubeyshivam9642 <115604495+dubeyshivam9642@users.noreply.github.com>
2022-11-16 18:14:29 +0530

    Add files via upload

---
+++ C - programming/Class work/arrdemo.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+/*
+
+Array : int mars[5];
+
+-Array is a name at memory location which stores multiple values of same type.
+- Array index starts with 0.
+
+
+*/
+
+
+void main()
+{
+	int mars[5] = {54,98,47,69,56};
+	
+	printf("%d\t",mars[0]);
+	printf("%d\t",mars[1]);
+	printf("%d\t",mars[2]);
+	printf("%d\t",mars[3]);
+	printf("%d\t",mars[4]);
+}
+
+
+
+
+
+
+
+
